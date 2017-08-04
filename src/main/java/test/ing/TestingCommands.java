3683cc3c9026f1457acdcb03b812f1e99985262e
Magnus <EnderCrypt@gmail.com>
2017-08-04 10:46:48 +0200

    added priority annotations to the test.ing classes

--- src/main/java/test/ing/TestingCommands.java
+++ src/main/java/test/ing/TestingCommands.java
@@ -4,11 +4,13 @@ import java.awt.Point;
 
 import net.ddns.endercrypt.intellicommand.CommandPath;
 import net.ddns.endercrypt.intellicommand.command.Include;
+import net.ddns.endercrypt.intellicommand.command.Priority;
 import net.ddns.endercrypt.intellicommand.command.CommandParam;
 
 @CommandPath("send")
 public class TestingCommands
 {
+	@Priority(1)
 	@CommandPath("message {number}")
 	public void message(@Include("key") Point key, @CommandParam("number") int text)
 	{
@@ -16,6 +18,7 @@ public class TestingCommands
 		System.out.println("NUMBER: " + text);
 	}
 
+	@Priority(0)
 	@CommandPath("message {text}")
 	public void message(@Include("key") Point key, @CommandParam("text") String text)
 	{
