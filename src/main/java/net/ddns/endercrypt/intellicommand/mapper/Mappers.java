6aa5245e0791a24f40530cc3c0a5a3c040c9424f
EnderCrypt <EnderCrypt@gmail.com>
2019-12-07 15:53:48 +0100

    properly added the file & path mapper

--- src/main/java/net/ddns/endercrypt/intellicommand/mapper/Mappers.java
+++ src/main/java/net/ddns/endercrypt/intellicommand/mapper/Mappers.java
@@ -1,10 +1,15 @@
 package net.ddns.endercrypt.intellicommand.mapper;
 
+
+import java.io.File;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 import net.ddns.endercrypt.intellicommand.exception.IntelliCommandException;
 import net.ddns.endercrypt.intellicommand.exception.MapperConversionFailed;
+import net.ddns.endercrypt.intellicommand.mapper.common.FileMapper;
+import net.ddns.endercrypt.intellicommand.mapper.common.PathMapper;
 import net.ddns.endercrypt.intellicommand.mapper.common.StringMapper;
 import net.ddns.endercrypt.intellicommand.mapper.common.primitive.BooleanMapper;
 import net.ddns.endercrypt.intellicommand.mapper.common.primitive.ByteMapper;
@@ -15,13 +20,14 @@ import net.ddns.endercrypt.intellicommand.mapper.common.primitive.IntegerMapper;
 import net.ddns.endercrypt.intellicommand.mapper.common.primitive.LongMapper;
 import net.ddns.endercrypt.intellicommand.mapper.common.primitive.ShortMapper;
 
+
 /**
  * @author EnderCrypt
  * 
- * class that holds all mappers and the class they produce
- * default mappers exists for all basic primitive datatypes +string
+ *         class that holds all mappers and the class they produce default
+ *         mappers exists for all basic primitive datatypes +string
  * 
- * strings can be converted into the correct object using the map method
+ *         strings can be converted into the correct object using the map method
  */
 public class Mappers
 {
@@ -38,10 +44,13 @@ public class Mappers
 		register(Short.class, new ShortMapper());
 		// common
 		register(String.class, new StringMapper());
+		register(File.class, new FileMapper());
+		register(Path.class, new PathMapper());
 	}
-
+	
 	/**
 	 * allows adding additional mappers
+	 * 
 	 * @param mapperType
 	 * @param mapper
 	 */
@@ -49,27 +58,29 @@ public class Mappers
 	{
 		mappers.put(mapperType, mapper);
 	}
-
+	
 	/**
 	 * attempts to convert a string into its mapper object
+	 * 
 	 * @param value
 	 * @param clazz
 	 * @return
-	 * @throws MapperConversionFailed if an internal mapper failed to map the correct object
-	 * @throws IntelliCommandException if no mapper was available to convert to this specific class
-	 * @throws IllegalArgumentException if an internal mapper converted to the wrong object class
+	 * @throws MapperConversionFailed
+	 *             if an internal mapper failed to map the correct object
+	 * @throws IntelliCommandException
+	 *             if no mapper was available to convert to this specific class
+	 * @throws IllegalArgumentException
+	 *             if an internal mapper converted to the wrong object class
 	 */
 	public Object map(String value, Class<?> clazz) throws MapperConversionFailed
 	{
 		ArgMapper mapper = mappers.get(clazz);
-		if (mapper == null)
-			throw new IntelliCommandException("Couldnt find mapper for class " + clazz.getSimpleName());
-
+		if (mapper == null) throw new IntelliCommandException("Couldnt find mapper for class " + clazz.getSimpleName());
+		
 		Object object = mapper.map(value);
-
-		if (clazz.isInstance(object) == false)
-			throw new IllegalArgumentException("Mapper for " + clazz + " returned an object of type " + object.getClass());
-
+		
+		if (clazz.isInstance(object) == false) throw new IllegalArgumentException("Mapper for " + clazz + " returned an object of type " + object.getClass());
+		
 		return object;
 	}
 }
